e2d4cca51acbd9b514287516db5ccb40adf11646
Nakassony <nakassony.bernardo@tistech.co.ao>
2021-06-29 15:50:30 +0100

    feat: Form content generate and submit to backend

--- service/index.js
+++ service/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = process.env.PORT | 5000;
 
 
 //app.use(require("body-parser")());
 //app.use(express.urlencoded());
-app.use(require("multer")().array())
+app.use(cors({origin: "*"}));
+app.use(require("multer")().array());
 
 app.listen(PORT, () => console.log(`Servico rodando em ${PORT}`));
 
@@ -14,7 +16,7 @@ app.listen(PORT, () => console.log(`Servico rodando em ${PORT}`));
 app.post("/newForm", (req, client) => {
 
     console.log(`Receivedd request is: `);
-    console.log(req.body.nome);
+    console.log(req.body);
 
     client.send("Process done!");
 
